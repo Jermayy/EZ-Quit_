0f383935bec611ba26beae7707a12ae575db60e4
Jeremy <jeremyVDLRC@gmail.com>
2020-10-24 19:01:42 +1100

    add progress bar to dashboard

--- client/src/Components/DashboardProgress/DashboardProgress.js
+++ client/src/Components/DashboardProgress/DashboardProgress.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import ProgressBar from 'react-bootstrap/ProgressBar';
 
 
 import './DashboardProgress.css'
 export class DashboardProgress extends Component{
+  
+  state = {
+    smokeLimit: "",
+    smokeCounter: "",
+    currentSmokeCount: ""
+  }
+  
+
+
     render(){
         return(
 <div className="dashboard">
@@ -14,11 +24,14 @@ export class DashboardProgress extends Component{
 <div className="dashboardBody">
         <Card id="counter">
             <Card.Body id="counterNumber">
-                <p>5</p>
+                <p>4</p>
             </Card.Body>
         </Card>
 
+        
 </div>
+<ProgressBar animated now={80} className="progressBar" label={`80%`} variant="danger"/>
+
 <Button variant="outline-info" className='btnInfo'>Edit Limit</Button>
 <div className="btnSmoke">
 <Button variant="danger" size="lg" block >
