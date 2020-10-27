bdfc026c9d434224e2f76e6d603422342e2843a0
Jeremy <jeremyVDLRC@gmail.com>
2020-10-27 22:38:00 +1100

    clearing directory

--- client/src/utils/actions.js
+++
@@ -1,2 +0,0 @@
-export const Create_Note = "Create_Note";
-export const Delete_Note = "Delete_Note"; 
--- routes/api/index.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const noteRoutes = require("./notes");
-
-// Note routes
-router.use("/notes", noteRoutes);
-
-module.exports = router;
