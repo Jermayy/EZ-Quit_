373dfb55cd6153b3249b1c6602af691b52633a9a
Jeremy <jeremyVDLRC@gmail.com>
2020-10-27 20:21:35 +1100

    list item card styling

--- client/src/Components/NoteList/NoteListItem.js
+++ client/src/Components/NoteList/NoteListItem.js
@@ -2,26 +2,38 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 
+import axios from 'axios';
+
 import "./NoteListItem.css";
 
-export const NoteListItem = ({ date, time, smoke, note }) => {
+export const NoteListItem = ({ _id,date, time, smoke, note }) => {
+
+
+async function deleteNote(){
+await axios.delete(`/api/notes/${_id}`);
+
+// window.location.reload();
+}
+
+
+
   return (
     <Card border="secondary" style={{ width: "18rem" }} className="noteCard">
       <Card.Body>
         <Card.Title className="noteInfo">
-          <div>
+          <div className="content">
             <div>Date : {date}</div>
             <div>Time : {time}</div>
             <div>No. of Smokes had : {smoke}</div>
           </div>
           <div className="noteButton">
-            <Button variant="danger" size="sm" id="button">
+            <Button variant="danger" size="sm" id="button" onClick={deleteNote}>
               Delete
             </Button>
           </div>
         </Card.Title>
 
-        <Card.Text>{note}</Card.Text>
+        <Card.Text className="body">{note}</Card.Text>
       </Card.Body>
     </Card>
   );
